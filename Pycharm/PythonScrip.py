eca3b6deca88e5f41fb46ec679bbfd8e7d26eef8
samprasgirt <>
2020-12-31 22:56:50 +0800

    add  python script

---
+++ Pycharm/PythonScrip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   ${NAME}.py    
+@Contact :   raogx.vip@hotmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+${DATE} ${TIME}   gxrao      1.0         None
+'''
+
+# import lib
+ 
+
